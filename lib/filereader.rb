d445a3d71e1dd7a3bdabe6b414e927cb893d7099
Myron <myron.yook@gmail.com>
2019-08-10 15:13:30 -0400

    work to geocode logic

--- lib/filereader.rb
+++ lib/filereader.rb
@@ -1,105 +1,36 @@
 # require 'byebug'
 
-# file = File.open('test.txt')
-
-# entries = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = { '00:00 - 04:00' => 0, '04:00 - 08:00' => 0, '08:00 - 12:00' => 0, '12:00 - 16:00' => 0, '16:00 - 20:00' => 0, '20:00 - 00:00' => 0  }}}}
-# exits = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = { '00:00 - 04:00' => 0, '04:00 - 08:00' => 0, '08:00 - 12:00' => 0, '12:00 - 16:00' => 0, '16:00 - 20:00' => 0, '20:00 - 00:00' => 0  }}}}
-# station_entries = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = { '00:00 - 04:00' => 0, '04:00 - 08:00' => 0, '08:00 - 12:00' => 0, '12:00 - 16:00' => 0, '16:00 - 20:00' => 0, '20:00 - 00:00' => 0  }}}
-# station_exits = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = { '00:00 - 04:00' => 0, '04:00 - 08:00' => 0, '08:00 - 12:00' => 0, '12:00 - 16:00' => 0, '16:00 - 20:00' => 0, '20:00 - 00:00' => 0  }}}
-# entry = [] 
-# exit = [] 
-# idx1 = 0 
-# idx2 = 0
-# previous_turnstile1 = "02-00-00"
-# previous_turnstile2 = "02-00-00"
-
-# file.each_line do |line| 
-#     array = line.split(',')
-#     station = array[3]
-#     turnstile = array[2]
-#     date = array[6]
-#     entry << array[-2].to_i
-#     exit << array[-1].to_i
-#     entries[station][turnstile][date]
-#     exits[station][turnstile][date]
-#     station_entries[station][date]
-#     station_exits[station][date]
-# end 
-
-# entries.each do |station, turnstiles| 
-#     turnstiles.each do |turnstile, dates|
-#         dates.each do |date, timeranges|
-#             timeranges.each do |timerange, counter|
-#                 if idx1 == 0
-#                     idx1 += 1
-#                     entries[station][turnstile][date][timerange] = entry[idx1] - entry[idx1-1]
-#                 elsif previous_turnstile1 != turnstile
-#                     idx1 += 2
-#                     previous_turnstile1 = turnstile
-#                     entries[station][turnstile][date][timerange] = entry[idx1] - entry[idx1-1]
-#                 else
-#                     break if entry[idx1+1].nil?
-#                     idx1 += 1 
-#                     entries[station][turnstile][date][timerange] = entry[idx1] - entry[idx1-1]
-#                 end 
-#             end 
-#         end 
-#     end 
-# end 
-
-# exits.each do |station, turnstiles| 
-#     turnstiles.each do |turnstile, dates|
-#         dates.each do |date, timeranges|
-#             timeranges.each do |timerange, counter|
-#                 if idx2 == 0
-#                     idx2 += 1
-#                     exits[station][turnstile][date][timerange] = exit[idx2] - exit[idx2-1]
-#                 elsif previous_turnstile2 != turnstile
-#                     idx1 += 2
-#                     previous_turnstile2 = turnstile
-#                     entries[station][turnstile][date][timerange] = entry[idx1] - entry[idx1-1]
-#                 else
-#                     break if exit[idx2+1].nil?
-#                     idx2 += 1
-#                     exits[station][turnstile][date][timerange] = exit[idx2] - exit[idx2-1]
-#                 end 
-#             end 
-#         end 
-#     end 
-# end 
-
-# station_turnstiles1 = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = { '00:00 - 04:00' => 0, '04:00 - 08:00' => 0, '08:00 - 12:00' => 0, '12:00 - 16:00' => 0, '16:00 - 20:00' => 0, '20:00 - 00:00' => 0  }}}
-# station_turnstiles2 = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = { '00:00 - 04:00' => 0, '04:00 - 08:00' => 0, '08:00 - 12:00' => 0, '12:00 - 16:00' => 0, '16:00 - 20:00' => 0, '20:00 - 00:00' => 0  }}}
-
-# entries.each do |station, turnstiles| 
-#     turnstiles.each do |turnstile, dates|
-#         dates.each do |date, timeranges|
-#             timeranges.each do |timerange, counter|
-#                 station_turnstiles1[station][date][timerange] += counter
-#             end 
-#         end 
-#     end 
-# end
-
-# exits.each do |station, turnstiles| 
-#     turnstiles.each do |turnstile, dates|
-#         dates.each do |date, timeranges|
-#             timeranges.each do |timerange, counter|
-#                 station_turnstiles2[station][date][timerange] += counter
-#             end 
-#         end 
-#     end 
-# end
-
-# p station_turnstiles1
-# p station_turnstiles2
-
 file = File.open('05182019.txt')
-# Count files, lump turnstiles (because we're going by station.)
 entries = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = Hash.new(0)}}
 exits = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = Hash.new(0)}}
-
 previous = nil
+timestamps = 
+    {'00' => '00:00 - 04:00',
+    '01' => '00:00 - 04:00',
+    '02' => '00:00 - 04:00',
+    '03' => '00:00 - 04:00',
+    '04' => '04:00 - 08:00',
+    '05' => '04:00 - 08:00',
+    '06' => '04:00 - 08:00',
+    '07' => '04:00 - 08:00',
+    '08' => '08:00 - 12:00',
+    '09' => '08:00 - 12:00',
+    '10' => '08:00 - 12:00',
+    '11' => '08:00 - 12:00',
+    '12' => '12:00 - 16:00',
+    '13' => '12:00 - 16:00',
+    '14' => '12:00 - 16:00',
+    '15' => '12:00 - 16:00',
+    '16' => '16:00 - 20:00',
+    '17' => '16:00 - 20:00',
+    '18' => '16:00 - 20:00',
+    '19' => '16:00 - 20:00',
+    '20' => '20:00 - 00:00',
+    '21' => '20:00 - 00:00',
+    '22' => '20:00 - 00:00',
+    '23' => '20:00 - 00:00'
+    }
+
 file.each_line do |line| 
     current = line.split(',')
     station = current[3]
@@ -113,11 +44,21 @@ file.each_line do |line|
         next
     else 
         date = previous[6] != date ? previous[6] : date 
-        timestamp = "#{previous[7]} - #{current[7]}"
+        previous_timestamp = previous[7]
+        timestamp = timestamps[previous_timestamp[0..1]]
         entries[station][date][timestamp] ||= 0
         exits[station][date][timestamp] ||= 0
-        entries[station][date][timestamp] += current[10].to_i - previous[10].to_i
-        exits[station][date][timestamp] += current[9].to_i - previous[9].to_i
+
+        previous_entries = previous[9].to_i
+        current_entries = current[9].to_i
+        previous_exits = previous[10].to_i
+        current_exits = current[10].to_i
+        if ((current_entries - previous_entries < 50000) && (current_entries - previous_entries > 0 ))
+            entries[station][date][timestamp] += current[9].to_i - previous[9].to_i
+        end 
+        if ((current_exits - previous_exits < 50000) && (current_exits - previous_exits > 0 ))
+            exits[station][date][timestamp] += current[10].to_i - previous[10].to_i
+        end 
         previous = current
     end
 end 
